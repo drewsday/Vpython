d92d666ff5964b693a41f11afc9dcf8a6c602a89
Andrew Morrison <amorriso@jjc.edu>
2014-07-27 11:18:22 -0500

    Finished the final version of the orbit program
    Added rotating Earth, vectors for force and velocity

--- voyager.py
+++ voyager.py
@@ -16,14 +16,21 @@ trail = curve(color=craft.color) ## for leaving a trail behind spacecraft
 vcraft = vector(0,2e3,0)
 pcraft = mcraft*vcraft
 t = 0
+#print('p=',pcraft)
+pointer = arrow(pos=craft.pos,color=color.green,axis=mag(pcraft)/3*norm(Earth.pos-craft.pos),shaftwidth=3)
+vpointer = arrow(pos=craft.pos,color=color.red,axis=pcraft/5,shaftwidth=3)
 
 #CALCULATION LOOP: ALL REPEATED CALCULATIONS GO HERE
 while t < 10*365*24*60*60:
     rate(100)
     force = G * mEarth * mcraft / mag(craft.pos)**2
-    Force = (force*(craft.pos-Earth.pos)/mag(craft.pos))
+    Force = (-force*(craft.pos-Earth.pos)/mag(craft.pos-Earth.pos))
     pcraft = pcraft + Force*deltat
     craft.pos = craft.pos + (pcraft/mcraft)*deltat
     trail.append(pos=craft.pos)
     t = t + deltat
-    
+    Earth.rotate(angle=pi/360,axis=(0,1,0),origin=Earth.pos)
+    pointer.pos=craft.pos
+    pointer.axis=mag(pcraft)/3*norm(Earth.pos-craft.pos)
+    vpointer.pos = craft.pos
+    vpointer.axis = pcraft/5
