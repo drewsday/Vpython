52991c05978baf630b2b68f1e2e4f1aac4d96350
Andrew Morrison <amorriso@jjc.edu>
2014-08-02 23:01:55 -0500

    Corrected vectors on spheres in 3spheres.py

--- 3spheres.py
+++ 3spheres.py
@@ -4,9 +4,9 @@ from visual import *
 #first program for students to do
 #helps introduce vectors in Vpython
 
-ball1 = sphere(pos=(10,10,0),color=color.green, radius=1)
-ball2 = sphere(pos=(-10,10,0),color=color.red, radius=1)
-ball3 = sphere(pos=(0,0,0),color=color.blue,radius=1)
+ball1 = sphere(pos=vector(10,10,0),color=color.green, radius=1)
+ball2 = sphere(pos=vector(-10,10,0),color=color.red, radius=1)
+ball3 = sphere(pos=vector(0,0,0),color=color.blue,radius=1)
 
 pointer1 = arrow(pos=(10,10,0),color=color.green,axis=(-20,0,0),shaftwidth=0.5)
 pointer2 = arrow(pos=(-10,10,0),color=color.red,axis=(10,-10,0),shaftwidth=0.5)
